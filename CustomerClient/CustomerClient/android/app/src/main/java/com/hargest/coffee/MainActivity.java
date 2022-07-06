7fef3e75614375e57c4e008f341832854543b9dc
JHC-16098 <16098@jhc.school.nz>
2022-07-07 10:21:48 +1200

    Set up Android build for capacitor

---
+++ CustomerClient/CustomerClient/android/app/src/main/java/com/hargest/coffee/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hargest.coffee;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- CustomerClient/CustomerClient/capacitor.config.ts
+++ CustomerClient/CustomerClient/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'CustomerClient',
+  appId: 'com.hargest.coffee',
+  appName: 'Hargest Coffee Company',
   webDir: 'build',
   bundledWebRuntime: false
 };
